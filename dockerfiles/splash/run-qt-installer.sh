1377d1eae6ed48ac413ad4ab1d639ccaab77c80f
Alex Khlivnuik <libraua@gmail.com>
2024-01-13 07:37:43 +0700

    Update run-qt-installer.sh

--- dockerfiles/splash/run-qt-installer.sh
+++ dockerfiles/splash/run-qt-installer.sh
@@ -12,5 +12,5 @@ chmod +x "$1" && \
 http_proxy="http://localhost:8080" https_proxy="http://localhost:8080" $command --script "$2" \
     | egrep -v '\[[0-9]+\] Warning: (Unsupported screen format)|((QPainter|QWidget))' && \
 ls /opt/qt-$QT_SHORT_VERSION/ && \
-#    cat /opt/qt-$QT_SHORT_VERSION/InstallationLog.txt && \
+cat /opt/qt-$QT_SHORT_VERSION/InstallationLog.txt && \
 cat /opt/qt-$QT_SHORT_VERSION/components.xml
